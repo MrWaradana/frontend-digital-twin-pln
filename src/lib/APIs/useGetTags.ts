21491529d27292300b9ede9f2410c497becba7bd
Ferdi761 <ferdiansyahachmad29@gmail.com>
2024-09-24 14:30:59 +0700

    update: heat loss tag chart

---
+++ src/lib/APIs/useGetTags.ts
@@ -0,0 +1,49 @@
+import { API_V1_LIVE_URL } from "../api-url";
+import { HookReply } from "./types";
+import { useApiFetch } from "./useApiFetch";
+import {format} from "date-fns";
+
+interface TagValues {
+    id: string;
+    tag_id: string;
+    time_stamp: string;
+    value: number;
+}
+
+interface Tags {
+    id: string;
+    descriptor: string | number;
+    digital_set_name: string | number;
+    display_digits: number;
+    engineering_units: string | number;
+    future: boolean | string;
+    name: string;
+    path: string;
+    point_class: string;
+    point_type: string;
+    span: number;
+    step: boolean;
+    values: TagValues[];
+    web_id: string;
+    zero: number;
+    // created_at: string;
+    // created_by: string;
+    // updated_at: string;
+    // updated_by: string;
+
+    // FOR LINECHART CONFIG
+    // color: string | null;
+    
+}
+// ${EFFICIENCY_API_URL}/variables?excel_id=${selectedExcel.id}
+
+export function useGetTags(
+    token: string | undefined,
+): HookReply<Array<Tags>> {
+
+    return useApiFetch(
+        `${API_V1_LIVE_URL}/tags`,
+        !!token,
+        token
+    )
+}
--- src/lib/api-url.ts
+++ src/lib/api-url.ts
@@ -1,3 +1,4 @@
 export const AUTH_API_URL = `${process.env.NEXT_PUBLIC_AUTH_APP_URL}`;
 export const EFFICIENCY_API_URL = `${process.env.NEXT_PUBLIC_EFFICIENCY_APP_URL}`;
+export const API_V1_LIVE_URL = `${process.env.NEXT_PUBLIC_API_V1_LIVE}`;
 const RCFA_API_URL = "http://";
