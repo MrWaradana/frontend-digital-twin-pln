ea04a8f29854a49f6156d7b8774a08e383a49910
MrWaradana <muhammadridhowaradana@gmail.com>
2024-10-10 17:18:48 +0700

    style: bar chart

--- src/components/efficiency-app/nett-plant-heat-rate/BarChartNPHR.tsx
+++ src/components/efficiency-app/nett-plant-heat-rate/BarChartNPHR.tsx
@@ -176,8 +176,8 @@ export default function BarChartNPHR({ data_id }: any) {
         </ModalContent>
       </Modal>
       <p className="w-full flex justify-center">
-        Selected Data:{" "}
-        <span className="font-bold pl-2"> {`${data?.name}`}</span>
+        Showing <span className="font-bold px-2"> {`${data?.name}`}</span>
+        Nett Plant Heat Rate Data
       </p>
       <Card>
         <CardHeader>
@@ -215,13 +215,21 @@ export default function BarChartNPHR({ data_id }: any) {
             </BarChart>
           </ChartContainer>
         </CardContent>
-        <CardFooter className="flex-col items-start gap-2 text-sm">
-          <div className="flex gap-2 font-medium leading-none">
-            Nett Plant Heat Rate Gap reached 250
+        <CardFooter className="flex-row items-start justify-between gap-2 text-sm">
+          <div>
+            <div className="flex gap-2 font-medium leading-none">
+              Nett Plant Heat Rate Gap reached{" "}
+              <span className="italic color-[#FF6961]">
+                {chartData[2].gap.toFixed(0)}
+              </span>
+            </div>
+            <div className="leading-none text-muted-foreground">
+              Showing {`${data?.name}`} Nett Plant Heat Rate Data
+            </div>
           </div>
-          <div className="leading-none text-muted-foreground">
-            Showing latest Nett Plant Heat Rate Data
-          </div>
+          <Button onClick={onOpen} className="bg-[#FF6961]">
+            Open Pareto Heat Loss
+          </Button>
         </CardFooter>
       </Card>
     </>
