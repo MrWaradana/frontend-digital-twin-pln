d308fb01df0df5c6fa585d940cfc790b11eb2b76
MrWaradana <muhammadridhowaradana@gmail.com>
2024-10-13 13:34:13 +0700

    feat: page input niaga

---
+++ src/app/efficiency-app/nett-plant-heat-rate/input-niaga/page.tsx
@@ -0,0 +1,9 @@
+import { EfficiencyContentLayout } from "../../../../containers/EfficiencyContentLayout";
+
+export default function Page() {
+  return (
+    <EfficiencyContentLayout title={"Input Niaga"}>
+      <div></div>
+    </EfficiencyContentLayout>
+  );
+}
--- src/app/efficiency-app/nett-plant-heat-rate/page.tsx
+++ src/app/efficiency-app/nett-plant-heat-rate/page.tsx
@@ -1,10 +1,24 @@
 // "use client";
 import { EfficiencyContentLayout } from "@/containers/EfficiencyContentLayout";
 import BarChartNPHR from "@/components/efficiency-app/nett-plant-heat-rate/BarChartNPHR";
+import { Button, Link } from "@nextui-org/react";
+import { PlusIcon } from "lucide-react";
 
 export default function Page() {
   return (
     <EfficiencyContentLayout title="Nett Plant Heat Rate">
+      {/* <Button
+        as={Link}
+        color="primary"
+        startContent={
+          <>
+            <PlusIcon />
+          </>
+        }
+        href="/efficiency-app/nett-plant-heat-rate/input-niaga"
+      >
+        Input Niaga
+      </Button> */}
       <div>
         <BarChartNPHR />
       </div>
