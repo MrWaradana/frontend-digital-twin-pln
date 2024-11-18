31525c205c790400e0cd66d0bf4a028ebbd335c0
perdanamain <firman.fp123@gmail.com>
2024-11-18 02:56:06 +0000

    refactor: PFI Layouts

--- src/components/pfi-app/nav/Sidebar.tsx
+++ src/components/pfi-app/nav/Sidebar.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Menu } from "@/components/pfi-app/nav/Menu";
-import { SidebarToggle } from "@/components/pfi-app/nav/SidebarToggle";
 import { Button } from "@/components/ui/button";
 import { useSidebarToggle } from "@/hooks/use-sidebar-toggle";
 import { useStore } from "@/hooks/use-store";
 import { cn } from "@/lib/utils";
 
-import PlnLogo from "../../../../public/Logo_PLN.svg";
+import PlnLogoBlack from "../../../../public/Logo_Black.png";
 
-export default function Sidebar() {
+
+export default function Sidebar({ className }: { className?: string }) {
   const sidebar = useStore(useSidebarToggle, (state) => state);
 
   if (!sidebar) return null;
@@ -18,11 +18,27 @@ export default function Sidebar() {
   return (
     <aside
       className={cn(
-        "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300 bg-gradient-to-b from-[#FFD9D3] to-white",
-        sidebar?.isOpen === false ? "w-[90px]" : "w-72"
+        "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
+        sidebar?.isOpen === false ? "w-[90px]" : "w-72",
+        `${className}`
       )}
     >
-      <Menu isListOpen={sidebar?.isOpen} />
+      {/* <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} /> */}
+      <div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto bg-transparent">
+        <Button
+          className={cn(
+            "transition-transform ease-in-out duration-300 mb-1 p-3",
+            sidebar?.isOpen === false ? "translate-x-1" : "translate-x-0"
+          )}
+          variant="link"
+          asChild
+        >
+          <Link href="#" className="flex items-center gap-2">
+            <Image src={PlnLogoBlack} alt="Logo PLN" />
+          </Link>
+        </Button>
+        <Menu isListOpen={sidebar?.isOpen} />
+      </div>
     </aside>
   );
 }
--- src/containers/PFILayout.tsx
+++ src/containers/PFILayout.tsx
@@ -5,18 +5,25 @@ import { cn } from "@/lib/utils";
 import Sidebar from "@/components/pfi-app/nav/Sidebar";
 import { useSidebarToggle } from "@/hooks/use-sidebar-toggle";
 
-export default function PFILayout({ children }: { children: React.ReactNode }) {
+export default function EfficiencyLayout({
+  children,
+  className = "bg-gradient-to-b from-[#FFD9D3] to-[#FFFFFF] to-[45%]",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   const sidebar = useStore(useSidebarToggle, (state) => state);
 
   if (!sidebar) return null;
 
   return (
     <>
-      <Sidebar />
+      <Sidebar className={className} />
       <main
         className={cn(
-          "min-h-[calc(100vh_-_56px)] bg-gradient-to-b from-[#FFD9D3] to-white dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
-          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
+          "min-h-[calc(100vh_-_56px)]  dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
+          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72",
+          `${className}`
         )}
       >
         {children}
